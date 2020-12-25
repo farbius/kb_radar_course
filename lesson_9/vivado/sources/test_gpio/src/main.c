3129e4ee8a5623014b40110657fcfa17d2db52ba
farbius <33464191+farbius@users.noreply.github.com>
2020-12-25 10:52:46 +0300

    lesson_9

--- lesson_9/vivado/sources/test_gpio/src/main.c
+++ lesson_9/vivado/sources/test_gpio/src/main.c
@@ -1,10 +1,9 @@
 /*
  * main.c
  *
- *  Created on: 22 дек. 2020 г.
+ *  Created on: 25 дек. 2020 г.
  *      Author: arost
  */
-
 #include "xparameters.h"
 #include "xil_exception.h"
 #include "xscugic.h"
@@ -12,6 +11,7 @@
 
 XScuGic Intc;
 XScuGic_Config *IntcConfig;
+
 
 #define XGPIO_DATA_OFFSET	0x0   /**< Data register for 1st channel */
 #define XGPIO_TRI_OFFSET	0x4   /**< I/O direction reg for 1st channel */
@@ -23,9 +23,11 @@ XScuGic_Config *IntcConfig;
 #define XGPIO_IER_OFFSET	0x128 /**< Interrupt enable register */
 
 
-unsigned int *gpio_address = (unsigned int *)XPAR_AXI_GPIO_0_BASEADDR;
+unsigned int *gpio0_address = (unsigned int *)XPAR_AXI_GPIO_0_BASEADDR;
+unsigned int *gpio1_address = (unsigned int *)XPAR_AXI_GPIO_1_BASEADDR;
 
-unsigned int gpio_data = 0, Intrflag = 0;
+unsigned int gpio_data = 0, Intrflag = 0, IntrflagSw = 0;
+
 
 void mem_set(unsigned int* mem_address, int offset, unsigned int value) {
 	mem_address[offset>>2] = value;
@@ -38,26 +40,33 @@ void mem_get(unsigned int* mem_address, int offset, unsigned int *value) {
 void GpioHandler()
 {
 	Intrflag = 1;
-	mem_get(gpio_address, XGPIO_DATA_OFFSET, &gpio_data);
-	mem_set(gpio_address, XGPIO_ISR_OFFSET, 0x01);
+	mem_set(gpio0_address, XGPIO_ISR_OFFSET, 0x01);
+}
+
+void GpioHandlerSw()
+{
+	IntrflagSw = 1;
+	mem_set(gpio1_address, XGPIO_ISR_OFFSET, 0x01);
 }
 
 int SetupIntrSystem();
 
+
 int main()
 {
-
 	xil_printf("Start Application \r\n");
 
-	// set GPIO
+	// setup GPIOs
 	xil_printf("Write to TRISTATE register \n\r");
-	mem_set(gpio_address, XGPIO_TRI_OFFSET, 0x01);
+	mem_set(gpio0_address, XGPIO_TRI_OFFSET, 0x01);
 
 	xil_printf("Write to IER register \n\r");
-	mem_set(gpio_address, XGPIO_IER_OFFSET, 0x01);
+	mem_set(gpio0_address, XGPIO_IER_OFFSET, 0x01);
+	mem_set(gpio1_address, XGPIO_IER_OFFSET, 0x01);
 
 	xil_printf("Write to GIE register \n\r");
-	mem_set(gpio_address, XGPIO_GIE_OFFSET, 0x80000000);
+	mem_set(gpio0_address, XGPIO_GIE_OFFSET, 0x80000000);
+	mem_set(gpio1_address, XGPIO_GIE_OFFSET, 0x80000000);
 
 	// Set intr controller
 	SetupIntrSystem();
@@ -66,22 +75,32 @@ int main()
 
 	xil_printf("Setup system \r\n");
 
-
-
-
-	while(1)
+	while(gpio_data != 0x0003)
 	{
+
 		if(Intrflag)
 		{
 			Intrflag = 0;
-			xil_printf("Get interrupt \n\r");
+			mem_get(gpio0_address, XGPIO_DATA_OFFSET, &gpio_data);
+			xil_printf("Get interrupt GPIO 0\n\r");
 			xil_printf("data reg is 0x%x \n\r", gpio_data);
-			mem_set(gpio_address, XGPIO_DATA2_OFFSET, gpio_data);
+			mem_set(gpio0_address, XGPIO_DATA2_OFFSET, gpio_data);
 		}
+
+
+		if(IntrflagSw)
+		{
+			IntrflagSw = 0;
+			mem_get(gpio1_address, XGPIO_DATA_OFFSET, &gpio_data);
+			xil_printf("Get interrupt GPIO 1\n\r");
+			xil_printf("data reg is 0x%x \n\r", gpio_data);
+		}
+
 
 
 	}
 
+	xil_printf("Exit Application \r\n");
 
 
 	return 0;
@@ -111,8 +130,17 @@ int SetupIntrSystem()
 		return Status;
 	}
 
+	Status = XScuGic_Connect(&Intc, XPAR_FABRIC_GPIO_1_VEC_ID, (Xil_ExceptionHandler)GpioHandlerSw, NULL);
+		if (Status != XST_SUCCESS) {
+			return Status;
+		}
+
+
+
+
 	/* Enable the interrupt for the GPIO device.*/
 	XScuGic_Enable(&Intc, XPAR_FABRIC_GPIO_0_VEC_ID);
+	XScuGic_Enable(&Intc, XPAR_FABRIC_GPIO_1_VEC_ID);
 
 	/*
 	 * Initialize the exception table and register the interrupt
